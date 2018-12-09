b39078db6aea002c8197944fa80bbf92db49c555
Jeremy Zhang <jeremy.zhang@smartcar.com>
2018-12-08 17:53:37 -0800

    Small fix

--- app/app/src/main/java/smartcar/com/getting_started_android_sdk/MainActivity.java
+++ app/app/src/main/java/smartcar/com/getting_started_android_sdk/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.util.Log;
 
 import com.smartcar.sdk.*;
 
