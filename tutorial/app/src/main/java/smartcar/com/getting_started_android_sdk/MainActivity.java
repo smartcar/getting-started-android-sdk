129ebc0dcfb5d60e3e55e8b1627a13c048bde9cc
Sanketh Katta <sanketh@smartcar.com>
2019-10-10 19:13:25 -0700

    Update tutorial to AndroidX.

--- tutorial/app/src/main/java/smartcar/com/getting_started_android_sdk/MainActivity.java
+++ tutorial/app/src/main/java/smartcar/com/getting_started_android_sdk/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 
 import com.smartcar.sdk.SmartcarAuth;
